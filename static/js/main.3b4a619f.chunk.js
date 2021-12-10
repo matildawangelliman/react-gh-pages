44ccabdcdd4fbb4800f1cdadbf2f3ee662ebc45a
matilda wang <matildawangelliman@gmail.com>
2021-12-09 21:37:38 -0500

    Updates

---
+++ static/js/main.3b4a619f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-gh-pages"]=this["webpackJsonpreact-gh-pages"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),o=(n(8),n(9),n(0));var i=function(){return Object(o.jsx)("div",{className:"App",children:"hello world123"})};a.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.3b4a619f.chunk.js.map
